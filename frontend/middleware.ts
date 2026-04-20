69d456d39fdd3ab2f6e7175eecef6834c3d178dd
ray.chunkit.chung <ray.chunkit.chung@gmail.com>
2026-04-20 20:36:08 +0900

    rename to proxy ts

--- frontend/middleware.ts
+++
@@ -1,6 +0,0 @@
-// TODO: Migrate to proxy.ts when next-auth adds support for Next.js 16 proxy convention
-export { auth as middleware } from "@/auth";
-
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-};
